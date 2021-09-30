29e08b93e21c1d300826333e0a70702638b07821
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-09-30 15:25:06 -0400

    use bulma

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Awesome SUNY New Paltz App"/>
+    <h1>This is actually the Home Page</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
