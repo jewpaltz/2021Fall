84e4b7c37de84e6ea9d8ce6419c6fe9c7f7ff471
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-10-07 12:04:02 -0400

    Binding

---
+++ client/src/services/session.js
@@ -0,0 +1,15 @@
+const session = {
+    user: null,
+    messages: [],
+    Login(){
+        this.user = {
+            FirstName: 'Moshe',
+            LastName: 'Plotkin',
+            id: 613,
+        }
+    }
+};
+
+export default session;
+
+//export function
