849698618e601462d0b82addc089dda0c9e22123
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-10-18 14:49:50 -0400

    messages component

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
         <div class="columns">
             <div class="column">
                 <Nav />
-
+                <messages />
                 <router-view/>                 
             </div>
         </div>
@@ -18,11 +18,13 @@
 </style>
 
 <script>
+import Messages from './components/Messages.vue';
 import Nav from "./components/Nav";
 
 export default {
     components: {
-        Nav
+        Nav,
+        Messages
     }
 }
 </script>
--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="section">
-      <h1 class="title">
-          Feed Page
-
-
-      </h1>
+      <h1 class="title"> Feed Page </h1>
+      
       <div class="columns">
         <div class="column is-one-third is-offset-one-third">
             
@@ -26,7 +23,9 @@ import session from "../services/session";
 import { GetWall } from "../services/posts";
 
 export default {
-  components: { Post },
+    components: {
+        Post
+    },
     data: ()=> ({
         posts: GetWall(session.user.handle)
     })
