a0e525f3552c461967f492dda6f8b97190375c81
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-10-21 14:53:59 -0400

    working api

---
+++ server/controllers/users.js
@@ -0,0 +1,13 @@
+/*  B"H
+*/
+const express = require("express");
+const model = require("../models/users");
+
+const app = express.Router();
+
+app
+    .get("/", (req, res, next) =>{
+        res.send(model.GetAll());
+    })
+
+module.exports = app;
