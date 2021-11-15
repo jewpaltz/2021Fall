5997534e521cf4fa740cb205da1afe79b267f6f3
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-11-15 15:14:52 -0500

    allow deleting of posts

--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -5,8 +5,8 @@
       <div class="columns">
         <div class="column is-one-third is-offset-one-third">
             
-            <div class="post" v-for="p in posts" :key="p.src">
-                <post :post="p" />
+            <div class="post" v-for=" (p, i) in posts" :key="p.src">
+                <post :post="p" @remove="remove(p, i)" />
             </div>
 
 
@@ -20,7 +20,7 @@
 <script>
 import Post from '../components/Post.vue';
 import session from "../services/session";
-import { GetFeed } from "../services/posts";
+import { Delete, GetFeed } from "../services/posts";
 
 export default {
     components: {
@@ -31,6 +31,15 @@ export default {
     }),
     async mounted(){
         this.posts = await GetFeed(session.user.handle)
+    },
+    methods: {
+        async remove(post, i){
+            console.log({post})
+            const response = await Delete(post.id)
+            if(response.deleted){
+                this.posts.splice(i, 1)
+            }
+        }
     }
 }
 
