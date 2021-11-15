ed1e539b83b45ad670fb63f1d9bf72c86934dc6b
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-11-15 14:41:10 -0500

    catch server errors

--- client/src/services/myFetch.js
+++ client/src/services/myFetch.js
@@ -7,7 +7,10 @@ const API_ROOT = process.env.VUE_APP_API_ROOT ?? 'http://localhost:3100/';
 
 export async function api(url){
     try {
-        const x = await fetch(API_ROOT + url);
+        const x = await fetch(API_ROOT + url + 'ff');
+        if(!x.ok){
+            throw await x.json();
+        }
         return await x.json();
     } catch (err) {
         session.Error(err);
