d8bfb03124853f23961a9467a9d4d6ee3fef936b
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-12-02 14:45:33 -0500

    added routes

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import logo from './assets/logo.svg';
+
 import './App.css';
 import Nav from "./components/Nav";
 import { BrowserRouter } from "react-router-dom";
+import OurRoutes from "./OurRoutes";
 
 function App() {
   return (
@@ -10,31 +11,9 @@ function App() {
     <div className="App">
 
         <Nav />
-        <section className="hero is-medium is-link">
-            <div className="hero-body">
-                <p className="title">
-                    Medium hero
-                </p>
-                <p className="subtitle">
-                    Medium subtitle
-                </p>
-            </div>
-        </section>
+        <OurRoutes />
+        
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
     </BrowserRouter>
   );
---
+++ react-client/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function(props){
+    return (
+    <section className="hero is-medium is-link">
+        <div className="hero-body">
+            <p className="title">
+                {props.title}
+            </p>
+            <p className="subtitle">
+                {props.subtitle}
+            </p>
+        </div>
+    </section>
+    )
+}
