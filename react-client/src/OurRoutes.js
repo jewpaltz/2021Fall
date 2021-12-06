11cb68d6e265af7ac56a1bee45f1cac975593073
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-12-06 15:16:13 -0500

    Feed Working

--- react-client/src/OurRoutes.js
+++ react-client/src/OurRoutes.js
@@ -3,14 +3,12 @@ import { Route, Routes } from "react-router";
 import Hero from "./components/Hero";
 import Post from "./components/Post";
 import session from "./services/session";
+import Feed from "./views/Feed";
 
 
 const About = ()=> <h1 className="title">Welcome to the About Page</h1>
 const Home = ()=> <Hero title="Home Page" subtitle="The main page" />
-const Feed = ()=> <Post post={ {
-    src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    user: session.user
- } } />
+
 const Users = ()=> <Hero title="Users Page" subtitle="The list of users page" />
 
 
